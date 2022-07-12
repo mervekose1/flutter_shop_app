140a40a4db48f7543afe331a2abe525385d25b51
Merve Köse <86488859+m-reactnative@users.noreply.github.com>
2022-07-12 22:11:52 +0300

    appbar done

--- lib/pages/Home_body.dart
+++ lib/pages/Home_body.dart
@@ -11,21 +11,83 @@ class _HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
-      body: Column(
-        children: [
-          // user row widget to create appbar
-          Row(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                ),
+              // user row widget to create appbar
+              Row(
+                // space between widgets
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    //  drawer is working :)
+                    child: Builder(
+                      builder: (context) => IconButton(
+                        icon: const Icon(Icons.dashboard),
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: const [
+                      Text(
+                        "User Name",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        "Location",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w700),
+                      ),
+                    ],
+                  ),
+                  const CircleAvatar(
+                    backgroundColor: Color.fromARGB(255, 228, 171, 196),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: const [
+                                BoxShadow(color: Colors.black38, blurRadius: 4)
+                              ],
+                            ),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                // remove the blue bar
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                ),
+                                prefixIcon: Icon(Icons.search),
+                              ),
+                            )),
+                      ),
+                    ],
+                  )
+                ],
               )
             ],
-          )
-        ],
+          ),
+        ),
       ),
       drawer: const Drawer(),
     );
