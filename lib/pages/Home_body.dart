5daebf771c374ba13a59debf8440c67bbfead2c3
Merve Köse <86488859+m-reactnative@users.noreply.github.com>
2022-07-13 23:49:58 +0300

    sorting done

--- lib/pages/Home_body.dart
+++ lib/pages/Home_body.dart
@@ -51,7 +51,7 @@ class _HomeBodyState extends State<HomeBody> {
                     ],
                   ),
                   const CircleAvatar(
-                    backgroundColor: Color.fromARGB(255, 228, 171, 196),
+                    backgroundColor: Color.fromARGB(255, 73, 243, 192),
                   ),
                 ],
               ),
@@ -61,25 +61,42 @@ class _HomeBodyState extends State<HomeBody> {
                     children: [
                       Expanded(
                         child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20),
-                              boxShadow: const [
-                                BoxShadow(color: Colors.black38, blurRadius: 4)
-                              ],
-                            ),
-                            child: const TextField(
-                              decoration: InputDecoration(
-                                // remove the blue bar
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide.none,
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide.none,
-                                ),
-                                prefixIcon: Icon(Icons.search),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: const [
+                              BoxShadow(color: Colors.black38, blurRadius: 4)
+                            ],
+                          ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              // remove the blue bar
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide.none,
                               ),
-                            )),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                              prefixIcon: Icon(Icons.search),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 73, 243, 192),
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: const [
+                            BoxShadow(color: Colors.black38, blurRadius: 4)
+                          ],
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.sort_by_alpha),
+                        ),
                       ),
                     ],
                   )
